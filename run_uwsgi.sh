b1a85de9430e9e72f7b286dd46d1b48b134d4eb0
Maciej Sawka <maciejsawka@gmail.com>
2021-01-10 15:34:13 +0100

    Fix script to make it portable to ubuntu (dirty hack)
    Fix script to use python3 or python module name depending on whether a file
    exists.
    
    Change psycopg2 to psycopg2-binary so that one does not need libpq-dev.

--- run_uwsgi.sh
+++ run_uwsgi.sh
@@ -4,7 +4,14 @@ if [ "$1" != 'prod' ]; then
     export FLASK_ENV="development"
 fi
 
-uwsgi --plugin python -H./venv/ \
+# dirty hack to make it switch between plugin python3 and python
+if [ -e "/usr/lib/uwsgi/python_plugin.so" ]; then
+    plugin_name="python"
+else
+    plugin_name="python3"
+fi
+
+uwsgi --plugin ${plugin_name} -H./venv/ \
     --wsgi-file wsgi.py --callable application \
     --static-map /static=./public_html \
-    --http localhost:5000
+    --http-socket localhost:5000
